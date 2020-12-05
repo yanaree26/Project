dc67ece551ef20ca694ec312b1f08b41b11b07bb
Yana Ree <yana261099@gmail.com>
2020-12-05 23:18:06 +0300

    save

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+    new Splide('.splide', {
+        type   : 'loop',
+        perPage: 3,
+        perMove: 1,
+        height : '100vh',
+        width: '100%',
+        focus  : 2,
+        updateOnMove: true,
+    }).mount();
+}
